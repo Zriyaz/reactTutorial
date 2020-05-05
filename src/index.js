cf9ad300cdddcab66cd373d2330c1bf647a83bd6
Md Riyaz Kazmi <mdriyazuddine17@gmail.com>
2020-05-05 06:19:17 +0000

    handleClick=()=>{     console.log("Button Clicked",this)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react"
-import ReactDOM from 'react-dom'
+import ReactDOM from "react-dom"
 import Student from "./Student"
-
 
 ReactDOM.render(<Student roll="14"/>,document.getElementById("root"))
