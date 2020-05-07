4fb960f89cc4011d5c778a5dd529eda89bff1056
Md Riyaz Kazmi <mdriyazuddine17@gmail.com>
2020-05-07 18:57:48 +0000

    //

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import App from "./App"
+import faker from "faker"
+import CommentDetail from "./CommentDetail"
 
-ReactDOM.render(<App/>,document.getElementById("root"))  
+const App=()=>{
+  return(
+    <div className="ui container comments">
+      <CommentDetail author="Md Riyaz"/>
+      <CommentDetail author="Ritesh kumar"/>
+      <CommentDetail author="Suraj Singh"/>
+      <CommentDetail author="kazmi"/>
+      <CommentDetail author="Pankaj"/>
+      <CommentDetail author="Maisha"/>
+      <CommentDetail author="Sarmila"/> 
+    </div>
+  )
+}
+
+ReactDOM.render(<App/>,document.querySelector("#root"))
