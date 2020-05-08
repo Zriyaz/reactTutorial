97520bcbaa17f7324392e32d21a6d8a3616e2e14
Md Riyaz Kazmi <mdriyazuddine17@gmail.com>
2020-05-08 04:41:46 +0000

    //

--- src/index.js
+++ src/index.js
@@ -6,13 +6,48 @@ import CommentDetail from "./CommentDetail"
 const App=()=>{
   return(
     <div className="ui container comments">
-      <CommentDetail author="Md Riyaz Ansari" TimeAgo="Todat at 4:36PM" Post="Hii Everyone" />
-      <CommentDetail author="Ritesh kumar singh" TimeAgo="Todat at 1:36PM" Post="kanha ho??"/>
-      <CommentDetail author="Suraj kumar Singh" TimeAgo="Yesterday at 5:6PM" Post="Riyazzzzz jeee" />
-      <CommentDetail author="Khatri" TimeAgo="Todat at 2:20PM" Post="Dubai me"/>
-      <CommentDetail author="Ankit Kumar Sinha" TimeAgo="Todat at 8:36AM" Post="sab log google meet pe aao jaldi"/>
-      <CommentDetail author="Manisha" TimeAgo="Todat at 1:00PM" Post="Hi"/>
-      <CommentDetail author="gowrav kumar Singh" TimeAgo="Todat at 4:340PM" Post="kya goro...riyaz"/> 
+      <CommentDetail
+       author="Md Riyaz Ansari"
+       TimeAgo="Todat at 4:36PM"
+       Post="Hii Everyone" 
+       avatar={faker.image.avatar()}
+       />
+      <CommentDetail
+       author="Ritesh kumar singh"
+       TimeAgo="Todat at 1:36PM" 
+       Post="kanha ho??"
+       avatar={faker.image.avatar()}
+       />
+      <CommentDetail
+       author="Suraj kumar Singh" 
+       TimeAgo="Yesterday at 5:6PM" 
+       Post="Riyazzzzz jeee" 
+       avatar={faker.image.avatar()}
+       />
+      <CommentDetail 
+       author="Khatri" 
+       TimeAgo="Todat at 2:20PM" 
+       Post="Dubai me"
+       avatar={faker.image.avatar()}
+       />
+      <CommentDetail 
+       author="Ankit Kumar Sinha" 
+       TimeAgo="Todat at 8:36AM" 
+       Post="sab log google meet pe aao jaldi"
+       avatar={faker.image.avatar()}
+       />
+      <CommentDetail
+       author="Sunny Kumar" 
+       TimeAgo="Todat at 1:00PM" 
+       Post="Hi"
+       avatar={faker.image.avatar()}
+       />
+      <CommentDetail
+       author="gowrav kumar Singh" 
+       TimeAgo="Todat at 4:340PM" 
+       Post="kya goro...riyaz"
+       avatar={faker.image.avatar()}
+       /> 
     </div>
   )
 }
